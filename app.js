373dbef33646d285f9f29ce1e5f993fade58b2c8
Bruce-jimenez <jimenezbruce2022@gmail.com>
2022-11-04 15:33:47 -0400

    add the enemy tags, placed in an array + more

--- app.js
+++ app.js
@@ -24,76 +24,106 @@ automatically attack the player after the player attacks
 */
 
 const enemies = [];
+const currentTarget = [];
+
 
 class playerShip {
     constructor(){
     this.hull = 20;
     this.firePower = 5;
     this.accuracy = .7;
+
+return "Health - " + playerShip.hull + ', Fire Power - ' + playerShip.firePower + ', Accuracy - ' + playerShip.accuracy
     }
 }
 
-class enemyShip{
-    constructor()
-
-}
-
+function createEnemy(name){
+    let shipName = name;
+   
     function hull(min, max){
         return Math.floor(Math.random()* (max - min) + min);
     }
-    
-    console.log(hull(3, 6))
-    
-    function firePower(min, max){
+           let hullHealth =  hull(3, 6);
+              
+         function firePower(min, max){
         return Math.floor(Math.random()* (max - min) + min);
+     }
+           let Power = firePower(2, 4);
+             
+         function accuracy(){
+        return (Math.floor(Math.random() * 3 + 6) / 10);
     }
-    
-    console.log(firePower(2, 4))
-    
-    function accuracy(min, max){
-        // return Math.floor(Math.random()* (max - min) + min);
-    }
+           let aim =  accuracy()
 
-    console.log(accuracy(.6, .8))
-
-
-
-
-
-// class enemyShip {
-//     constructor(){
-
-//     }
-// }
-
-
-function attack (){
-    if(/* Player ship is greater than enemy ship*/){
-        // Player fire power is subtracted from the hull of the enemy 
-    } 
-    else(){
-
-    }
-    else if (//enemy ship is greater than enemy ship){
-        // Enemy fire power is subtracted from the hull of the enemy
-    }
-     
-        
-
-        
-    
+     return shipName + " : " + "Health - " + hullHealth + ", Damage - " + Power + ", Accuracy - " + aim
+            
 }
 
 
-function retreat (){
+let enemy1 = createEnemy('Destroyer');
+let enemy2 = createEnemy('Alpha');
+let enemy3 = createEnemy('Beta');
+let enemy4 = createEnemy('Zeta');
+let enemy5 = createEnemy('Urnas');
+let enemy6 = createEnemy('Appolo');
+let enemy7 = createEnemy('Zeena');
 
+let USS_Schwarzenegger = new playerShip;
+
+let health = USS_Schwarzenegger.hull;
+let damage = USS_Schwarzenegger.firePower;
+let sight = USS_Schwarzenegger.accuracy;
+
+document.querySelector(".playerStats").innerHTML = 'Hull: ' +  health, "Fire Power: " + damage, 'Accuracy: ' + sight; 
+
+
+
+// console.log(enemy1);
+// console.log(enemy2);
+// console.log(enemy3);
+// console.log(enemy4);
+// console.log(enemy5);
+// console.log(enemy6);
+// console.log(enemy7);
+
+enemies.push(enemy1)
+enemies.push(enemy2)
+enemies.push(enemy3)
+enemies.push(enemy4)
+enemies.push(enemy5)
+enemies.push(enemy6)
+enemies.push(enemy7)
+
+console.log(enemies)
+
+console.log(USS_Schwarzenegger);
+
+
+// function attack (){
+//     if(/* Player ship is greater than enemy ship*/){
+//         // Player fire power is subtracted from the hull of the enemy 
+//     } 
+//     else(){
+
+//     }
+//     else if (//enemy ship is greater than enemy ship){
+//         // Enemy fire power is subtracted from the hull of the enemy
+//     }
+
+
+    
+// }
+
+
+function retreat (/*The pattern should be when the enemyship is 0  */ ){
+//It should be 
 }
 
 function onWards(){
 
 }
 
-
+//when the enemyship pass 7 ships the whole code as a whole should produce a modal where it states that it is over now
 //What modal would it be should it reset or pause 
 
 
